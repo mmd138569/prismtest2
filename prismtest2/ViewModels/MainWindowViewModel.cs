b72753214d190a55f2eacf711dc06c2b046ce63a
mmd <muhammad138569@gmail.com>
2024-03-12 16:00:23 +0330

    Revert "first comit"
    This reverts commit 059bc4fd9d68bc7fdac25a8f8e8f05736df1786a.

--- prismtest2/ViewModels/MainWindowViewModel.cs
+++ prismtest2/ViewModels/MainWindowViewModel.cs
@@ -9,7 +9,6 @@ namespace prismtest2.ViewModels
     {
         private string _title = "Prism Application";
         public string Title
-
         {
             get { return _title; }
             set { SetProperty(ref _title, value); }
