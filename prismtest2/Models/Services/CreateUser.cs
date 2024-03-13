96fcbec0fb1fee3fff62d220ca6ad460f120dcd5
mmd <muhammad138569@gmail.com>
2024-03-13 10:41:47 +0330

    create user

---
+++ prismtest2/Models/Services/CreateUser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+using Microsoft.EntityFrameworkCore;
+using prismtest2.Models.Context;
+
+namespace prismtest2.Models.Services
+{
+    public class CreateUser : ICreateUser
+    {
+        private readonly DataBaseContext context;
+        public CreateUser()
+        {
+            context = new DataBaseContext();
+        }
+
+        public List<UserListDTO> userListDtos()
+        {
+            var users = context.Dbusers.Select(p => new UserListDTO
+            {
+                id=p.Id,
+                Username = p.Username,
+                Password = p.Password
+            }).ToList();
+            return users;
+        } 
+    }
+}
