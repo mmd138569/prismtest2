2942ee13cf21ca947cccda7f7b2bcdbce4bb7964
mmd <muhammad138569@gmail.com>
2024-03-26 16:42:15 +0330

    try to add the numbers to the list

--- prismtest2/ViewModels/PrismUserControl2ViewModel.cs
+++ prismtest2/ViewModels/PrismUserControl2ViewModel.cs
@@ -9,13 +9,14 @@ using System.Diagnostics;
 using System.Linq;
 using prismtest2.ViewModels;
 using Telerik.Windows.Controls;
+using Telerik.Windows.Documents.Spreadsheet.Expressions.Functions;
 
 namespace prismtest2.ViewModels
 {
     public class PrismUserControl2ViewModel : BindableBase
     {
         private IRegionManager _regionManager;
-        public ObservableCollection<Gray> listofInputs;
+        public ObservableCollection<double> listofInputs { get; set; }
 
         public PrismUserControl2ViewModel(IRegionManager regionManager)
         {
@@ -35,20 +36,36 @@ namespace prismtest2.ViewModels
         {
             //listofInputs = listofinputs;
             ArrivalsPerHour = new ObservableCollection<double>();
-            ArrivalsPerHour.AddRange(listofinputs);
+            listofInputs = new ObservableCollection<double>();
 
-            for (int hour = 0; hour <= 255; hour++)
+                listofInputs.AddRange(listofinputs);
+                int[] a = new int[256];
+                for (int i = 0; i < 256; i++)
+                {
+                    a[i] = 0;
+                }
+            for (int hour = 0; hour <= listofinputs.Count; hour++)
             {
-                int itemsCount = rnd.Next(1, 5);
-                if (hour >= 8 && hour < 16)
-                {
-                    itemsCount = rnd.Next(5, 10);
-                }
+                //int itemsCount = rnd.Next(1, 5);
+                //if (hour >= 8 && hour < 16)
+                //{
+                //    itemsCount = rnd.Next(5, 10);
+                //}
 
-                for (int k = 0; k < itemsCount; k++)
+                //for (int k = 0; k < itemsCount; k++)
+                //{
+                //    ArrivalsPerHour.Add(hour);
+                //}
+                if (listofInputs.Count != 0)
                 {
-                    ArrivalsPerHour.Add(hour);
+                    int convertor = (int)listofInputs[hour];
+                    a[convertor] = a[convertor] + 1;
                 }
+            }
+
+            if (a[0] != 0)
+            {
+                ArrivalsPerHour.AddRange(a);
             }
         }
 
